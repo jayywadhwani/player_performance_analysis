2cddfbfbdaa06fbb50a52f92aba6bd3d43d5d25c
jayywadhwani <jaywadhwani6263779835@gmail.com>
2025-04-27 17:56:26 +0530

    frontend updated with python edits

--- app.py
+++ app.py
@@ -11,8 +11,24 @@ def results():
       test_performance_chart,odi_performance_chart,t20_performance_chart,pie_test_chart,pie_odi_chart,pie_t20_chart =player_performance_analysis_total()
       chart_test_teamwise,chart_odi_teamwise,chart_t20_teamwise=player_performance_analysis_teamwise("v England")
       if request.method == 'POST':
+            if request.form.get('BF-input') == None or request.form.get('BF-input')==  "" or request.form.get('BF-input')== "NULL":
+                  match_type = ""
+                  balls_faced = 0
+                  strike_rate = 0
+                  inngs_no = 0
+                  predicted_runs = 0
+                  player_performance_analysis_prediction(match_type,balls_faced,strike_rate,inngs_no)
+            else:
+                  match_type = request.form['format-input']
+                  balls_faced = request.form['BF-input']
+                  strike_rate = request.form['SR-input']
+                  inngs_no = request.form['Inngs-input']
+                  predicted_runs=player_performance_analysis_prediction(match_type,balls_faced,strike_rate,inngs_no)
+            test_performance_chart,odi_performance_chart,t20_performance_chart,pie_test_chart,pie_odi_chart,pie_t20_chart =player_performance_analysis_total(player_name)
+      if request.method == 'POST':
             player_name = request.form['opposition-input']
             chart_test_teamwise,chart_odi_teamwise,chart_t20_teamwise=player_performance_analysis_teamwise(player_name)
+
 
       return render_template('index.html',avg_run_test=avg_run_test,avg_run_odi=avg_run_odi,avg_run_t20=avg_run_t20,
       avg_sr_test=avg_sr_test,avg_sr_odi=avg_sr_odi,avg_sr_t20=avg_sr_t20,
@@ -20,7 +36,9 @@ def results():
       test_performance_chart=test_performance_chart,
       odi_performance_chart=odi_performance_chart,t20_performance_chart=t20_performance_chart,
       pie_test_chart=pie_test_chart,pie_odi_chart=pie_odi_chart,pie_t20_chart=pie_t20_chart,
-      chart_test_teamwise=chart_test_teamwise,chart_odi_teamwise=chart_odi_teamwise,chart_t20_teamwise=chart_t20_teamwise)
+      chart_test_teamwise=chart_test_teamwise,
+      chart_odi_teamwise=chart_odi_teamwise,
+      chart_t20_teamwise=chart_t20_teamwise,predicted_runs=predicted_runs)
 
 
 if __name__ == "__main__":
